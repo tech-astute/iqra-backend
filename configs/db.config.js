6bc8b8fb35fcbfe3cbc648a4792532f1ed3e0525
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-12-21 21:03:52 +0530

    resources removed

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,23 +1,8 @@
-// module.exports = {
-//   host: process.env.HOST,
-//   user: process.env.USER,
-//   password: process.env.PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.PORT,
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
-
 module.exports = {
-  host: "localhost",
-  user: "root",
-  password: "Root@123",
-  database: "testingiqra",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB_NAME,
   port: process.env.PORT,
   dialect: "mysql",
   pool: {
@@ -27,3 +12,18 @@ module.exports = {
     idle: 10000,
   },
 };
+
+// module.exports = {
+//   host: "localhost",
+//   user: "root",
+//   password: "Root@123",
+//   database: "testingiqra",
+//   port: process.env.PORT,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
